73fe19fb5f613295e5c3cda5363b03ad7af5f602
lf2186 <linfei3389@qq.com>
2015-02-04 15:04:31 +0800

    201502041504

---
+++ lfinflux-cli/lftest.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	//"bufio"
+	//"os"
+	//"fmt"
+	"github.com/influxdb/influxdb/client"
+	"log"
+)
+
+func main() {
+	cli, err := client.NewClient(&client.ClientConfig{
+		Host:     "127.0.0.1:8086",
+		Database: "test",
+		Username: "root",
+		Password: "wifi123",
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	series := &client.Series{
+		Name:    "test",
+		Columns: []string{"time", "value"},
+		Points: [][]interface{}{
+			[]interface{}{1394398326, 7},
+		},
+	}
+
+	if err := cli.WriteSeriesWithTimePrecision([]*client.Series{series}, "s"); err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+	   scanner := bufio.NewScanner(os.Stdin)
+
+	   for scanner.Scan() {
+	       fmt.Println(scanner.Text())
+	   }
+
+	   if err := scanner.Err(); err != nil {
+	       log.Fatal(err)
+	   }
+	*/
+}
